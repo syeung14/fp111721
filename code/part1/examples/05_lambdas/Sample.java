b95eae6561a1f731f677c66514561d77570b9952
venkats@agiledeveloper.com <venkats@agiledeveloper.com>
2021-11-17 06:34:35 -0700

    lambdas

---
+++ code/part1/examples/05_lambdas/Sample.java
@@ -0,0 +1,25 @@
+public class Sample {
+  public static void main(String[] args) {
+    //functions
+    //1. name
+    //2. return type
+    //3. parameter list
+    //4. body
+
+    //lambda expressions are anonymous:
+    //1. parameter list
+    //2. body
+    //a separator that sets the parameter list from the body
+
+    //Java: (parameter list) -> body
+    //Scala, C#, JavaScript: (parameter list) => body
+    //Python: lambda parameter list: body
+    //Ruby: |parameter list| body
+    //Haskell: \parameter list ... body
+
+
+    //lambdas may be passed to functions or even stored into variables
+    //and can be created within functions
+  }
+}
+
