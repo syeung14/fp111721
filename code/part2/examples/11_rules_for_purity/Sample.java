e7f1b09bcab949fea1eaa558fc946411e1bd996d
venkats@agiledeveloper.com <venkats@agiledeveloper.com>
2021-11-17 09:06:32 -0700

    rules for purity

---
+++ code/part2/examples/11_rules_for_purity/Sample.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.util.stream.*;
+
+public class Sample {
+  public static void main(String[] args) {
+    //A pure function does not have any side-effects.
+    //A pure function will give the same output for the same input, no matter how
+    //may times we call it.
+   
+    //lambdas and closures *have* to be pure, even when a language does not protect us.
+
+    //Two rules for purity:
+    //1. A pure function should not change any externally visible state
+    //2. A pure function should not depend on any external state that may possibly change
+  }
+}
+
